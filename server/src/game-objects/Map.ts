83d7498ef351ff05ef61bcc05279edd257896480
Pamperin, Clint A <c.pamperin21@gmail.com>
2016-10-22 13:19:38 -0500

    add: name prop to map

--- server/src/game-objects/Map.ts
+++ server/src/game-objects/Map.ts
@@ -1,9 +1,11 @@
 import {Tile} from './Tile';
 
 export class Map {
+    public name: string;
     public tiles: Tile[][];
 
-    constructor(tiles: Tile[][]) {
+    constructor(name: string, tiles: Tile[][]) {
+        this.name = name;
         this.tiles = tiles;
     }
 }
