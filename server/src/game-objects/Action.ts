db3e48feddeeabc51404ff0dbd20c23152c05ffa
Clint Pamperin <c.pamperin21@gmail.com>
2016-10-22 22:47:05 -0500

    add: iaction and actionresult

--- server/src/game-objects/Action.ts
+++ server/src/game-objects/Action.ts
@@ -1,13 +1,25 @@
 import {Command} from './Command';
 import {Noun} from './Noun';
 import {Item} from './Item';
+import {Position} from './Position';
+import {Phase} from './Phase';
 
 export class Action {
     public commands: Command;
     public noun: Noun;
+    public phase: Phase;
 
-    constructor (commands: Command, noun: Noun | Item) {
+    constructor (commands: Command, noun: Noun | Item, phase?: Phase) {
         this.commands = commands;
         this.noun = noun;
+        this.phase = phase;
     }
+}
+
+export class ActionResult {
+    public position: Position;
+}
+
+export interface IAction {
+    perform: () => ActionResult;
 }
