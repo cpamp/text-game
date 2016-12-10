cd9d484cc21b651d15c9cd4dd9c6071dfd795929
Pamperin, Clint A <c.pamperin21@gmail.com>
2016-12-10 14:39:55 -0600

    add: phase preview

--- server/src/game-objects/Phase.ts
+++ server/src/game-objects/Phase.ts
@@ -4,10 +4,12 @@ export class Phase {
     public children: Phase[];
     public description: string;
     public actions: Action[];
+    public preview: string;
 
-    constructor(children: Phase[], description: string, actions: Action[]) {
+    constructor(children: Phase[], description: string, actions: Action[], preview: string) {
         this.children = children;
         this.description = description;
         this.actions = actions;
+        this.preview = preview;
     }
 }
